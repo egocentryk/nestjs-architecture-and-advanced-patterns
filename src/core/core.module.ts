8870700a5d53d48f62ae5ba94ee407e429b4764a
egocentryk <lukasz.skowron@gmail.com>
2025-03-10 11:59:58 +0100

    feat(): hexagonal architecture in practice #2

---
+++ src/core/core.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ApplicationBootstrapOptions } from '../common/interfaces/application-bootstrap-options.interface'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { configSchema } from 'src/config.schema'
+
+@Module({})
+export class CoreModule {
+  static forRoot(options: ApplicationBootstrapOptions) {
+    const imports = [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env',
+        validationSchema: configSchema,
+      }),
+      ...(options.driver === 'orm'
+        ? [
+            TypeOrmModule.forRootAsync({
+              imports: [ConfigModule],
+              useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('DATABASE_HOST'),
+                port: parseInt(
+                  configService.get('DATABASE_PORT') || '5432',
+                  10,
+                ),
+                username: configService.get('DATABASE_USER'),
+                password: configService.get('DATABASE_PASSWORD'),
+                database: configService.get('DATABASE_NAME'),
+                autoLoadEntities: true,
+                synchronize: true,
+              }),
+              inject: [ConfigService],
+            }),
+          ]
+        : []),
+    ]
+
+    return {
+      module: CoreModule,
+      imports,
+    }
+  }
+}
